f7eb87047992b1afb23b50a3e7b566c7c752ea3d
Rohit Patil <rohit.patil@mitaoe.ac.in>
2024-07-04 23:03:06 +0530

    Add files via upload

---
+++ src/components/userDetails.js
@@ -0,0 +1,36 @@
+// src/data/userDetails.js
+
+const userDetails = {
+  studentId: "123456",
+  collegeName: "XYZ College",
+  rollNo: "78910",
+  passingYear: "2024",
+  branch: "Computer Science",
+  degree: "B.Tech",
+  phoneNumber: "+91 8261074745",
+  email: "rohit@example.com",
+  linkedin: "https://www.linkedin.com/in/thenameisrohhit",
+  name: "Rohit Patil",
+  dob: "January 17, 2003",
+  gender: "Male",
+  alternateEmail: "rohit@example.com",
+  parents: "X, Y",
+  address: "Pune, Maharashtra, India",
+  skills: ["Java", "Python", "C++", "HTML", "CSS", "JavaScript", "React"],
+  projects: [
+    {
+      name: "Online Examination System",
+      description: "Developed a web-based online examination system using Java and MySQL."
+    },
+    {
+      name: "Personal Portfolio Website",
+      description: "Created a personal portfolio website using React and CSS to showcase my projects and skills."
+    },
+    {
+      name: "Library Management System",
+      description: "Implemented a library management system using Python and SQLite for managing book inventories."
+    }
+  ]
+};
+
+export default userDetails;
